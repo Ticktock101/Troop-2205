c2964d4baec910f1b45114d1e715d0b1c6a246cb
Omkar Rayudu <omkarrayudu@gmail.com>
2023-05-30 17:17:14 -0500

    slideshow works

--- script/index.js
+++ script/index.js
@@ -1,18 +1,3 @@
-const images = [
-    './images/image_2.jpg',
-    './images/image_7.JPG',
-    './images/image_4.JPG',
-    './images/image_5.JPG',
-  ];
-  
-  let currentIndex = 0;
-  const backgroundContainer = document.querySelector('.background-image');
-  
-  function changeBackgroundImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    backgroundContainer.style.backgroundImage = `url(${images[currentIndex]})`;
-  }
-  
-  // Call the function at a specific interval to change the background image
-  setInterval(changeBackgroundImage, 5000); // Change image every 5 seconds
-  
+$('.carousel').carousel({
+    interval: 2000
+  })
