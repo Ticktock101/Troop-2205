12d58c9f003f5fa580bde733993ae00094c3853b
Omkar Rayudu <omkarrayudu@gmail.com>
2023-06-06 16:02:54 -0500

    fixed some things

--- script/high-adventure.js
+++ script/high-adventure.js
@@ -1,3 +1,12 @@
 $('.carousel').carousel({
     interval: 2000
 })
+
+function rotateArrow(event) {
+    event.preventDefault();
+  
+    var arrowImage = document.querySelector('.arrow-image');
+    arrowImage.classList.toggle('rotate-arrow');
+  }
+  
+  
